cf1e16cdf66a99b1455fb8010a7d141e26a9428b
Lemsito22 <lemquilca22@gmail.com>
2026-01-16 13:43:40 +0100

    ...

--- JAVA/src/tresenRaya.java
+++ JAVA/src/tresenRaya.java
@@ -11,23 +11,78 @@ public class tresenRaya {
             for (int j = 0; j < defaultLado; j++) {
                 tablero[i][j]="-";
             }
-        }
-        //Creación de Personaje ---------------------------------------------------------------------------------
+        } //ESTA PARTE ES LA ASIGNACIÓN DE VALOR EN EL TABLERO
+        //CREACION DEL PERSONAJE ---------------------------------------------------------------------------------
         String [] players = new String[2];
         System.out.println("3 EN RAYA");
         System.out.println("JUGADOR 1:");
         players[0]=sc.next();
         System.out.println("JUGADOR 2:");
         players[1]=sc.next();
-        //-------------------------------------------------------------------------------------------------------
+        //REGLAS DE JUEGO Y SELECCION DEL JUGADOR QUE COMIENZA----------------------------------------------------
+
+
+        int jugadoraleatorio= random.nextInt(2);
+        System.out.println("EXPLICACIÓN: PARA JUGAR (O) o (X) DEBES DE HACER CON COORDENADAS.");
+        System.out.println("EL TABLERO COMIENZA EN 'O' HORIZONTALMENTE Y TERMINA EN '"+(defaultLado-1)+"' Y LO MISMO OCURRE DE FORMA VERTICAL");
+        System.out.println("EJEMPLO DE UN TURNO: 0 1, 0 2, 1 1");
         System.out.println("TABLERO");
         for (int i = 0; i < defaultLado; i++) {
             for (int j = 0; j < defaultLado; j++) {
                 System.out.print(" "+tablero[i][j]+" ");
             }
             System.out.println();
-        } //ESTA PARTE ES LA ASIGNACIÓN DE VALOR EN EL TABLERO/ IMPRIMIR EL TABLERO
-        int jugadoraleatorio= random.nextInt(2);
+        } //IMPRIMIR EL TABLERO
         System.out.println("COMIENZA: "+players[jugadoraleatorio]);
+        //VARIABLES PARA EL JUEGO --------------------------------------------------------------------------------
+        boolean juego=true;
+        int posicionF = 0;
+        int posicionC=0;
+        //BUCLE JUEGO --------------------------------------------------------------------------------------------
+        while (juego) {
+            boolean jugador1 = true;
+            while (jugador1) {
+                System.out.println("ESCOGE LA POSICION DONDE QUIERES PONER (O)");
+                posicionF = sc.nextInt();
+                posicionC = sc.nextInt();
+                //------------------------------------------------------------------------------------------ PLAYER 1
+                for (int i = 0; i < defaultLado; i++) {
+                    for (int j = 0; j < defaultLado; j++) {
+                        if (i == posicionF && j == posicionC) {
+                            tablero[i][j] = "O";
+                        }
+                    }
+                }
+                for (int i = 0; i < defaultLado; i++) {
+                    for (int j = 0; j < defaultLado; j++) {
+                        System.out.print(" " + tablero[i][j] + " ");
+                    }
+                    System.out.println();
+                } //---------------------------ACTUALIZA EL TABLERO
+                break;
+            }
+            //-------------------------------------------------------------------------------------------PLAYER 2
+            boolean jugador2 = true;
+            while (jugador2) {
+                System.out.println("ESCOGE LA POSICION DONDE QUIERES PONER (X)");
+                posicionF = sc.nextInt();
+                posicionC = sc.nextInt();
+
+                for (int i = 0; i < defaultLado; i++) {
+                    for (int j = 0; j < defaultLado; j++) {
+                        if (i == posicionF && j == posicionC) {
+                            tablero[i][j] = "X";
+                        }
+                    }
+                }
+                for (int i = 0; i < defaultLado; i++) {
+                    for (int j = 0; j < defaultLado; j++) {
+                        System.out.print(" " + tablero[i][j] + " ");
+                    }
+                    System.out.println();
+                } //---------------------------ACTUALIZA EL TABLERO
+                break;
+            }
+        }
     }
 }
